b76e6dc3bc5bf3ed5c4d62ccdf37c6244c3f2d88
Andrew Principato <princian007@gmail.com>
2022-09-20 20:12:06 -0500

    Added node.js skeleton along with test base client

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Skeletons
+# # Skeletons
 Skeletons of various client and server code that can serve as a foundation for future projects
+
+# Client
+1) React
+2) Typescript
+3) Vanilla
+
+# Server
+1) C#
+2) Node.js
---
+++ server/node/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//Files
+const PATH_TO_CLIENT = '/Users/andrew/Documents/Skeletons/client/vanilla';
+const index = 'index.html';
+
+//ENV Config
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+})
+//app.render(index);
+
+app.get('/', function(req, res) {
+  //res.sendFile(path.join(__dirname, index));
+  res.sendFile(path.join(PATH_TO_CLIENT, index));
+});
+
+//Routes
+var exampleRouter = require('./routers/Router_Example.js');
+
+app.use('/example', exampleRouter);
